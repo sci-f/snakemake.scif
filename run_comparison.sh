a2c5fee5408c210accdd972c945a0b6e7eb2c289
Vanessa Sochat <vsochat@stanford.edu>
2018-03-09 20:11:49 -0500

    adding container friends

--- run_comparison.sh
+++ run_comparison.sh
@@ -1,16 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script assumes that we start in the same directory as this script,
 # and will run and save results for different pipelines (to compare later)
 
-# If running interactively, use HERE=$PWD
-HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+# usageL source run_comparison.sh
+
+HERE=$PWD
 DATA=$HERE/data
 RESULT=$HERE/results
 
 mkdir -p $RESULT
 
 function clean () {
+    echo
     echo "Cleaning up previous results..."
     rm -rf $HERE/data/sorted_reads
     rm -rf $HERE/data/mapped_reads
@@ -22,43 +24,53 @@ function clean () {
 
 function saveResult() {
     container=$1
+    echo "Saving $container results..."
     output=${RESULT}/${container}
     mkdir -p $output
     cp $DATA/calls/all.vcf $output/calls.vcf
     cp $DATA/report.html $output/report.html
     cp $DATA/genome.fa.* $output/
     cp $DATA/dag.svg $output/
+    echo
 }
 
+function introduce() {
+    container=$1
+    clean
+    cat $HERE/asciinema/$container.txt
+    echo
+    echo "Running $container pipeline"
+    sleep 2
+}
+
+cat asciinema/intro.txt
+sleep 3
+
 # Run for Docker first
-clean
-echo "Running docker pipeline"
-docker build --no-cache -t vanessa/snakemake.scif .
+introduce docker
+#docker build --no-cache -t vanessa/snakemake.scif .
 docker run -v $PWD/data:/scif/data -v $PWD:/scif/data/snakemake -it vanessa/snakemake.scif run snakemake all
 docker run -v $PWD/data:/scif/data -v $PWD:/scif/data/graphviz_create_dag -it vanessa/snakemake.scif run graphviz_create_dag /scif/data/graphviz_create_dag report.html dag.svg
+sudo chown -R vanessa data/
 saveResult docker
-sudo chown -R vanessa data
 
 # Singularity
-clean
-echo "Running singularity pipeline"
-sudo singularity build snakemake.simg Singularity
+introduce singularity
+#sudo singularity build snakemake.simg Singularity
 singularity run --bind data/:/scif/data snakemake.simg run snakemake all
 singularity run --bind data/:/scif/data snakemake.simg run graphviz_create_dag $PWD report.html dag.svg
 saveResult singularity
 
 # Charliecloud
-clean
-echo "Running charliecloud pipeline"
+introduce charliecloud
 ch-run --cd $PWD -b data:/scif/data /var/tmp/snakemake.ch -- /opt/conda/bin/scif run snakemake all
 ch-run --cd $PWD -b data:/scif/data /var/tmp/snakemake.ch -- /opt/conda/bin/scif run graphviz_create_dag $PWD report.html dag.svg
 saveResult charliecloud
 
 # Runc
-clean
-echo "Running runc pipeline"
+introduce runc
 mkdir -p /tmp/runc/rootfs
-sudo singularity build --sandbox /tmp/runc/rootfs snakemake.simg
+#sudo singularity build --sandbox /tmp/runc/rootfs snakemake.simg
 cp configs/runc-config-noninteractive.json /tmp/runc/config.json
 runc --root /tmp/runc run --bundle /tmp/runc snakmake.runc
 cp configs/runc-config-dag.json /tmp/runc/config.json
@@ -66,7 +78,18 @@ sudo chown -R $USER /tmp/runc/rootfs
 runc --root /tmp/runc run --bundle /tmp/runc snakmake.runc
 saveResult runc
 
+cat asciinema/goals.txt
+sleep 2
+cat asciinema/goalsfor.txt
+sleep 2
+cat asciinema/reproducibility.txt
+sleep 2
+cat asciinema/portability.txt
+sleep 2
+cat asciinema/friends.txt
+sleep 2
+
 # Shifter
-clean
-echo "Shifter pipeline run on cluster"
-echo "shifter --image=vanessa/snakemake.scif --volume `pwd`/data:/scif/data --workdir /scif/data /opt/conda/bin/scif run snakemake all"
+#clean
+#echo "Shifter pipeline will be run on cluster!"
+#echo "shifter --image=vanessa/snakemake.scif --volume `pwd`/data:/scif/data --workdir /scif/data /opt/conda/bin/scif run snakemake all"
